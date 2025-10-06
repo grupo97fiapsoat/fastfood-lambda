1c16c4476367e5f1190ba6c333f36dfcc000803f
Amanda Cardoso <amandabocardoso0@gmail.com>
2025-10-06 15:46:43 -0300

    deploy aws att

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,10 @@
-# Role usado pela Lambda para execução
-resource "aws_iam_role" "lambda_exec" {
-  name = "fastfood-lambda-exec-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+data "aws_iam_role" "lambda_exec" {
+  name = "fastfood-lambda-github-role"
 }
 
-# Anexa a policy básica (logs etc.)
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Criação (ou atualização) da função Lambda
 resource "aws_lambda_function" "fastfood_lambda" {
   function_name = "fastfood-lambda"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = data.aws_iam_role.lambda_exec.arn
   handler       = "handler.main"
   runtime       = "python3.13"
 
